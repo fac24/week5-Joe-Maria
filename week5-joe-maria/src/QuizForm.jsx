0d15a8930689da881f111c2126e7c94534e50509
= <maria.louisa.castro@gmail.com>
2022-06-08 15:51:39 +0100

    Create form and set states based on user input
    Relates #3

--- week5-joe-maria/src/QuizForm.jsx
+++ week5-joe-maria/src/QuizForm.jsx
@@ -1,6 +1,63 @@
 import React from "react";
 
 export default function QuizForm(props) {
-  console.log(props);
-  return <p>Hello from Form</p>;
+  const categories = [
+    { name: "Animals", id: "21" },
+    { name: "World Capitals", id: "78" },
+    { name: "Nature", id: "267" },
+    { name: "Musical Instruments", id: "184" },
+    { name: "Food & Drink", id: "253" },
+  ];
+
+  function handleGameState(e) {
+    e.preventDefault();
+    !props.gameState ? props.setGameState(true) : props.setGameState(false);
+  }
+
+  // If quiz is in process don't show the form
+  if (props.gameState) {
+    return null;
+    // If game is not in process show the form
+  } else {
+    return (
+      <form
+        // On form submit toggle the game state
+        onSubmit={handleGameState}
+      >
+        <fieldset>
+          <legend>Create your quiz:</legend>
+          {categories.map((cat) => {
+            return (
+              <label htmlFor={cat.name} key={cat.id}>
+                <input
+                  type="radio"
+                  name="categories"
+                  id={cat.id}
+                  value={cat.id}
+                  checked={cat.id === props.category}
+                  onChange={(e) => props.setCategory(e.target.value)}
+                />
+                {cat.name}
+              </label>
+            );
+          })}
+          <br></br>
+
+          <label htmlFor="quiz-length">
+            How many questions would you like?
+            <input
+              required
+              type="number"
+              id="quiz-length"
+              min="1"
+              max="20"
+              onChange={(e) => props.setQuizLength(e.target.value)}
+            ></input>
+          </label>
+          <br></br>
+          <button type="submit">Generate quiz</button>
+        </fieldset>
+      </form>
+    );
+  }
 }
